59eca907653b28958f11bb0586c98b4aa2ea8112
Timelaying <makanjuola.timi@gmail.com>
2024-07-20 14:39:06 +0100

    forms complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import { Routes, Route } from 'react-router-dom';
 
 import './globals.css';
 import React from 'react'
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
   <main className="flex h-screen">
     <Routes>
     {/*public route */}
-    <Route path="/sign-in" element={<SigninForm/>} />
+      <Route element={<AuthLayout/>}>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+      </Route>
 
     {/*private route */}
-    <Route index element={<Home />} />
+    <Route element={<RootLayout />}>
+      <Route index element={<Home />} />
+
+    </Route>
 
 
     </Routes>
